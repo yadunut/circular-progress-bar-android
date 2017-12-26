0ae6c2758510447e21de46983f4a458b3e0f2119
Yadunand <yadunut@gmail.com>
2017-12-27 00:51:13 +0530

    Added example

--- app/src/main/java/yadunut/github/com/circularprogressbarandroid/MainActivity.kt
+++ app/src/main/java/yadunut/github/com/circularprogressbarandroid/MainActivity.kt
@@ -2,11 +2,27 @@ package yadunut.github.com.circularprogressbarandroid
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import yadunut.github.com.circularprogressbar.CircleProgress
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var progressBar: CircleProgress
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        progressBar = findViewById(R.id.progress_bar)
+        Timer().schedule(Test({progressBar.progress += 1}), 1000L, 10L)
+
     }
+
+    class Test(var function: () -> Unit) : TimerTask() {
+
+        override fun run() {
+            function()
+        }
+
+    }
+
 }
